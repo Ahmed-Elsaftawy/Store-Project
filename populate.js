994de72abbba5477d764486ebe7d92533b230835
Ahmed <gamerahmed933@gmail.com>
2026-05-23 02:24:02 +0300

    prepare the json data 1

--- populate.js
+++ populate.js
@@ -1,2 +1,5 @@
 import { config } from "dotenv";
+import { connectDb } from "./index.js";
+import data from "./products.json" assert  {type: "json"};
+import { productModle } from "./models/product-model.js";
 config();
